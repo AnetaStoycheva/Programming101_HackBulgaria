3824ed30ed9f77c8d47e529ddcb7bad264707560
Ani <ani.stoicheva@gmail.com>
2015-06-04 00:15:19 +0300

    Replacing SELECT(*) with column names.

--- Week_8/pure_SQL.sql
+++ Week_8/pure_SQL.sql
@@ -1,10 +1,3 @@
--- import sqlite3
-
-
--- connection = sqlite3.connect("hr.db")
--- cursor = connection.cursor()
-
-
 -- List all employees with their first name, last name and title
 SELECT FirstName, LastName, Title
 FROM Employees
@@ -31,14 +24,18 @@ WHERE Title LIKE '%Sales%'
 -- List all females employees that work in the Sales department.
 SELECT FirstName, LastName, Title, TitleOfCourtesy
 FROM Employees
-WHERE Title LIKE '%Sales%' AND TitleOfCourtesy LIKE '%s%'
+WHERE Title LIKE '%Sales%'
+AND TitleOfCourtesy LIKE '%s%'
+-- AND TitleOfCourtesy IN ('Ms.', 'Mrs.')
 
 
 -- List the 5 oldest employees.
 SELECT FirstName, LastName, BirthDate
 FROM Employees
-ORDER BY BirthDate
+ORDER BY BirthDate ASC
 LIMIT 5
+-- ASC - vry6ta v namalqva6t red
+-- ima LIMIT 2, 5 -> t.e. pyrvite 5, zapo4vajki ot vtoriq
 
 
 -- List the first 5 hires of the company.
@@ -56,26 +53,28 @@ WHERE ReportsTo IS NULL
 
 -- List all employes by their first and last name, and the first and last name of the employees that they report to.
 SELECT e.FirstName, e.LastName, e.ReportsTo, managers.FirstName, managers.LastName
-FROM Employees as e
-JOIN Employees as managers
+FROM Employees AS e
+JOIN Employees AS managers
 ON e.ReportsTo = managers.EmployeeID
-WHERE e.ReportsTo IS NOT NULL
+-- WHERE e.ReportsTo IS NOT NULL  -> 6efyt go nqma v rezultata taka ili ina4e, za6toto st-ta mu e Null
 
 
 -- Count all female employees.
-SELECT COUNT(*)
+SELECT COUNT(EmployeeID)
 FROM Employees
-WHERE TitleOfCourtesy LIKE '%s%'
+WHERE TitleOfCourtesy
+IN ('Ms.', 'Mrs.')
 
 
 -- Count all male employees.
-SELECT COUNT(*)
+SELECT COUNT(EmployeeID)
 FROM Employees
 WHERE TitleOfCourtesy NOT LIKE '%s%'
 
 
 -- Count how many employees are there from the different cities. For example, there are 4 employees from London.
-SELECT City, COUNT(*)
+-- V COUNT moje da slojim vmesto City - Employee ID
+SELECT City, COUNT(City)
 FROM Employees
 GROUP BY City
 
@@ -95,13 +94,13 @@ ON ShipVia = ShipperID
 
 
 -- List all contries and the total number of orders that are going to be shipped there.
-SELECT ShipCountry, COUNT(*)
+SELECT ShipCountry, COUNT(OrderID) AS OrdersNumbers
 FROM Orders
 GROUP BY ShipCountry
 
 
 -- Find the employee that has served the most orders.
-SELECT Orders.EmployeeID, COUNT(*) AS order_count, FirstName, LastName
+SELECT Orders.EmployeeID, FirstName, LastName, COUNT(OrderID) AS order_count
 FROM Orders
 JOIN Employees
 ON Employees.EmployeeID = Orders.EmployeeID
